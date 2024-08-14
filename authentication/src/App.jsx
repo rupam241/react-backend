27bb7d24461beb8654add2421b5eca89c9cd5935
Rupam Dey <rupamdey241@gmail.com>
2024-08-14 22:56:45 +0530

    re

--- authentication/src/App.jsx
+++ authentication/src/App.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+import {login,logout} from './store/authSlice'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading,setLoading]=useState(true);
+
+  const dispatch=useDispatch();
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }
+
+      else{
+        dispatch(logout({}))
+      }
+    })
+    .finally(()=>{
+      setLoading(falses)
+    })
+  },[])
   
-  return (
-    <div>App</div>
-  )
+  return !loading? (
+    <div ></div>
+  ) :null
 }
 
-export default App
+export default App 
